ca6899c73644a502f0349e05d497c230688d7941
Melanie Miguel Villavicencio <80484383+AndromedasMami@users.noreply.github.com>
2021-05-25 23:25:11 -0400

    Refactored both addEventListener callbacks. Created a function that takes a string and returns a number that we interpolate into the getElementById function.

--- script.js
+++ script.js
@@ -2,124 +2,41 @@ const boxOne = ["1", "2", "3", "4", "5", "q", "w","e", "r", "t"]
 const boxTwo = ["6", "7", "8", "9", "0", "p", "o","i", "u", "y"]
 const boxThree = ["a", "s", "d", "f", "g", "z", "x","c", "v", "b"]
 const boxFour = ["h", "j", "k", "l", ";", "\'", "n","m", ",", "."]
-const posIndex = [1, 2, 3, 4]
 
-// for (var i = 0; i < posIndex.length; i++) {
-//     revealAnimal(document.getElementById(`cover${posIndex[i]}`));
-// }
-
-
-    document.addEventListener("keydown", (e) => {
-       console.log(e.key)
-
-       if (boxOne.includes(e.key)){
-        console.log("Key was pressed inside box 1.")
-        let coverElement = document.getElementById("cover1")
-        
-        if (coverElement.className === "cover") {
-            coverElement.className = "uncover"
-        }
-       }
-
-       if (boxTwo.includes(e.key)){
-        console.log("Key was pressed inside box 2.")
-        let coverElement = document.getElementById("cover2")
-        
-        if (coverElement.className === "cover") {
-            coverElement.className = "uncover"
-        }
-       }
-       if (boxThree.includes(e.key)){
-        console.log("Key was pressed inside box 3.")
-        let coverElement = document.getElementById("cover3")
-        
-        if (coverElement.className === "cover") {
-            coverElement.className = "uncover"
-        }
-       }
-       if (boxFour.includes(e.key)){
-        console.log("Key was pressed inside box 4.")
-        let coverElement = document.getElementById("cover4")
-        
-        if (coverElement.className === "cover") {
-            coverElement.className = "uncover"
+    function getBoxNumber(keyValue) {
+        if (boxOne.includes(keyValue)){
+            return 1
+        }if (boxTwo.includes(keyValue)){
+            return 2
+        }if (boxThree.includes(keyValue)){
+            return 3
+        }if (boxFour.includes(keyValue)){
+            return 4
         }
     }
+   
+    document.addEventListener("keydown", (e) => {
+       console.log(e.key)
+        const boxNumber = getBoxNumber(e.key)
+
+        console.log(`Key was pressed inside box ${getBoxNumber(e.key)}.`)
+        let coverElement = document.getElementById(`cover${getBoxNumber(e.key)}`)
+        
+        if (coverElement.className === "cover") {
+            coverElement.className = "uncover"
+        }
     })
 
     document.addEventListener("keyup", (e) => {
         console.log(e.key)
- 
-        if (boxOne.includes(e.key)){
-         console.log("Key was unpressed inside box 1.")
-         let coverElement = document.getElementById("cover1")
+        const boxNumber = getBoxNumber(e.key)
+
+        console.log(`Key was unpressed inside box ${boxNumber}.`)
+        let coverElement = document.getElementById(`cover${boxNumber}`)
      
-         if (coverElement.className === "uncover") {
+        if (coverElement.className === "uncover") {
              coverElement.className = "cover"
-         }
- 
         }
-        if (boxTwo.includes(e.key)){
-            console.log("Key was unpressed inside box 2.")
-            let coverElement = document.getElementById("cover2")
-        
-            if (coverElement.className === "uncover") {
-                coverElement.className = "cover"
-            }
-    
-           }
-           if (boxThree.includes(e.key)){
-            console.log("Key was unpressed inside box 3.")
-            let coverElement = document.getElementById("cover3")
-        
-            if (coverElement.className === "uncover") {
-                coverElement.className = "cover"
-            }
-    
-           }
-           if (boxFour.includes(e.key)){
-            console.log("Key was unpressed inside box 4.")
-            let coverElement = document.getElementById("cover4")
-        
-            if (coverElement.className === "uncover") {
-                coverElement.className = "cover"
-            }
-    
-           }
-     
-     })
-
-    //  document.addEventListener("keydown", (e) => {
-    //     console.log(e.key)
- 
-    //     if (boxTwo.includes(e.key)){
-    //     //  console.log("Key was pressed inside box 2.")
-    //      let coverElement = document.getElementById("cover2")
-     
- 
-    //      if (coverElement.className === "cover") {
-    //          coverElement.className = "uncover"
-    //      }
- 
-    //     }
-     
-    //  })
- 
-    //  document.addEventListener("keyup", (e) => {
-    //      console.log(e.key)
-  
-    //      if (boxTwo.includes(e.key)){
-    //     //   console.log("Key was unpressed inside box 2.")
-    //       let coverElement = document.getElementById("cover2")
       
-  
-    //       if (coverElement.className === "uncover") {
-    //           coverElement.className = "cover"
-    //       }
-  
-    //      }
-      
-    //   })
- 
-
-
+    })
+        
